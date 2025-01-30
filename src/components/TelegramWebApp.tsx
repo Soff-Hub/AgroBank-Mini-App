482fb5ce1ae3c0e9dd062aaf2ae506fec53b8ce1
Dilmurod <dilmurodcode@gmail.com>
2025-01-30 15:13:50 +0500

    ss

--- src/components/TelegramWebApp.tsx
+++ src/components/TelegramWebApp.tsx
@@ -110,7 +110,7 @@ const CameraComponent: React.FC = () => {
       formData.append("latitude", location?.latitude?.toString() || "");
       formData.append("longitude", location?.longitude?.toString() || "");
   
-      const response = await axios.post("http://192.168.1.15:8000/api/v1/common/blank/", formData, {
+      const response = await axios.post("http://bank.soffhu.uz/api/v1/common/blank/", formData, {
         headers: {
           "Content-Type": "multipart/form-data", 
         },
