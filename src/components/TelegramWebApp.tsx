e424a3db9450fb93e02c207838645eb3eec8fb81
Ozodbek <abdisamatovozodbek003@gmail.com>
2025-02-07 14:48:29 +0500

    text updates

--- src/components/TelegramWebApp.tsx
+++ src/components/TelegramWebApp.tsx
@@ -31,6 +31,7 @@ const CameraComponent: React.FC = () => {
   const hi = window.location?.search
   const token = hi?.split("?hi=")[1]
   const [toggle, setToggle] = useState<"user" | "environment">("user");
+  const [successText, setSuccessText] = useState("")
 
 
   async function getBrandCrums() {
@@ -175,6 +176,7 @@ const CameraComponent: React.FC = () => {
       });
       if (response.status === 201) {
         toast.success("Anketa muvaffaqiyatli to'ldirildi!");
+        setSuccessText("Anketa mal'umotlari muvaffaqiyatli yuborildi!")
         setLocationAllowed(false);
         setPhotoTaken(false);
         setVideoAllowed(false);
@@ -259,129 +261,9 @@ const CameraComponent: React.FC = () => {
 
 
 
-  return (<>
-    {
-      loadPermes !== 200 ?
-        (<>
-          <Toaster />
-          {tabNumberContinues &&
-            <div style={{ width: "100%", }}>
-              <Input placeholder="Anketa ID" style={{ width: "100%", height: "39.5px", marginBottom: "10px" }} onChange={(e) => setTabNumber(e.target.value)} value={tabNumber} />
-              <button disabled={Boolean(!tabNumber)} onClick={() => { setTabNumberContinues2(true), setTabNumberContinues(false) }} style={{ padding: "12px 20px", width: "100%", borderRadius: "10px", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", cursor: Boolean(!tabNumber) ? "not-allowed" : "pointer" }}>
-                Davom etish <i className="fa-solid fa-arrow-right"></i>
-              </button>
-            </div>
-          }
-          {tabNumberContinues2 &&
-            <div style={{ width: "100%", textAlign: "center", display: "flex", flexDirection: "column", gap: "5px" }}>
-              <Breadcrumb
-                items={items}
-              />
-              <h3 style={{ margin: "10px" }}>Holatlar</h3>
-              {
-                data?.results?.map((item: any) => (
-                  <div onClick={() => {
-                    if (item?.children) {
-                      setDataPathFilter(item?.id);
-                      setStatusID('');
-                    } else {
-                      setStatusID(item);
-                    }
-                  }} style={{ backgroundColor: statusID?.id === item?.id ? "rgba(111, 120, 127, 0.1)" : "", cursor: "pointer", width: '100%', border: "1px solid rgba(0,0,0,0.1) ", padding: "10px 0", marginBottom: "5px", borderRadius: "8px", position: "relative" }}>
-                    {item?.name}
-                    {item?.children && <span style={{ position: "absolute", top: "12px", right: "15px" }}><i className="fa-solid fa-chevron-down"></i></span>}
-                  </div>
-                ))
-              }
-              {statusID?.id &&
-                <>
-                  {(statusID?.requirement && statusID?.requirement !== "not_money_and_date") && <div style={{ width: "100%" }}>
-                    <DatePicker placeholder=" Sana tanlash" onChange={onChange} onFocus={() => setErrors((prev: any) => ({ ...prev, payment_date: "" }))} style={{ height: "39.5px", marginBottom: "5px", width: "100%" }} />
-                    {errors?.payment_date && <p style={{ color: "red", margin: "0", textAlign: "start" }}>{errors?.payment_date}</p>}
-                  </div>}
-
-                  {(statusID?.requirement && statusID?.requirement === "ten_day_in_month") && <div style={{ marginBottom: "10px" }}>
-                    <InputNumber onFocus={() => setErrors((prev: any) => ({ ...prev, payment_amount: "" }))} type="number" placeholder="To'lov summasi" value={price} onChange={(e) => setPrice(e)} style={{ height: "39.5px", marginBottom: "3px", width: "100%" }} />
-                    {errors?.payment_amount && <p style={{ color: "red", margin: "0", textAlign: "start" }}>{errors?.payment_amount}</p>}
-
-                  </div>}
-
-                  <div style={{ marginBottom: "10px" }}>
-                    <TextArea
-                      onFocus={() => setErrors((prev: any) => ({ ...prev, comment: "" }))}
-                      showCount
-
-                      onChange={(e) => setComment(e?.target?.value)}
-                      placeholder="Izoh 10 ta belgidan katta bo'lsin"
-                      style={{ height: 120, resize: 'none', marginBottom: "3px" }}
-                    />
-                    {errors?.comment && <p style={{ color: "red", margin: "0", textAlign: "start" }}>{errors?.comment}</p>}
-                  </div>
-
-                  <button disabled={loading} onClick={uploadToServer} style={{ padding: "12px 20px", width: "100%", borderRadius: "10px", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", cursor: loading ? "not-allowed" : "pointer" }}>
-                    {loading && <i className="fa-solid fa-spinner"></i>} Yuborish <i className="fa-solid fa-paper-plane"></i>
-                  </button>
-                </>
-              }
-            </div>}
-
-          {
-            <div style={{
-              textAlign: "center",
-              gap: "10px",
-              width: "100%",
-              display: (!tabNumberContinues2 && !tabNumberContinues) ? "grid" : "none"
-
-            }}>
-              {<video ref={videoRef} autoPlay playsInline style={{ width: "100%", display: (videoAllowed && !photoTaken) ? "block" : "none", borderRadius: "10px" }}></video>}
-
-              {<canvas ref={canvasRef} style={{ width: "100%", display: photoTaken ? "block" : "none", borderRadius: "10px" }}></canvas>}
-
-              {error && <p style={{ color: "red", }}>{error}</p>}
-              {(!locationAllowed && !videoAllowed) && <p style={{
-                backgroundColor: "#E5E5FF",
-                padding: "20px 0",
-                margin: "0 auto",
-                borderRadius: "10px",
-                color: "#7F4DFF",
-                width: "100%"
-              }}>
-                Joylashuv ma'lumotlarini berishga rozimisiz?</p>}
-
-
-              {(locationAllowed && videoAllowed) ? (
-                <div style={{ display: "flex", gap: "10px", justifyContent: "center", width: "100%" }}>
-                  {photoTaken ? <>
-                    <button onClick={() => startCamera()} style={{ padding: "12px 20px", width: "100%", borderRadius: "10px", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", display: "flex", gap: "5px", justifyContent: "center", alignItems: "center", cursor: "pointer" }}>
-                      <i className="fa-solid fa-camera-rotate"></i> <span style={{ whiteSpace: "nowrap" }}>Kameraga qaytish</span>
-                    </button>
-                    <button onClick={() => { setTabNumberContinues(true), stopCamera() }} style={{ padding: "12px 20px", width: "100%", borderRadius: "10px", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", cursor: "pointer" }}>
-                      {!!tabNumber && <i className="fa-solid fa-spinner"></i>} Davom etish <i className="fa-solid fa-arrow-right"></i>
-                    </button>
-                  </> :
-                    <div style={{ display: "flex", justifyContent: "space-between", width: "100%", gap: "10px" }}>
-                      <button onClick={captureImage} style={{ padding: "12px 20px", borderRadius: "10px", width: "100%", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", cursor: "pointer" }}>
-                        <i className="fa-solid fa-camera-retro"></i> Rasm olish
-                      </button>
-                      <button onClick={() => {
-                        setToggle(toggle === "user" ? "environment" : "user");
-                        startCamera();
-                      }}
-
-                        style={{ padding: "12px 20px", borderRadius: "10px", width: "100%", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", cursor: "pointer", display: "flex", justifyContent: "center", gap: "5px" }}>
-                        <i className="fa-solid fa-camera-retro"></i> <span style={{ whiteSpace: "nowrap" }}>Kamerani almashtirish</span>
-                      </button>
-                    </div>
-                  }
-                </div>
-              ) :
-                <button onClick={onPermissionChange} style={{ padding: "12px 20px", borderRadius: "10px", width: "100%", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", cursor: "pointer", display: "flex", gap: "5px", justifyContent: "center", alignItems: "center", }}>
-                  <i className="fa-solid fa-camera-rotate"></i> <span style={{ whiteSpace: "nowrap" }}>Ruxsat berish</span>
-                </button>
-              }
-            </div>
-          }
-        </>) :
+  return (
+    <>
+      {successText ?
         <>
           <div style={{
             textAlign: "center",
@@ -390,18 +272,174 @@ const CameraComponent: React.FC = () => {
             display: "grid"
 
           }}>
-            <p style={{
-              backgroundColor: "#E5E5FF",
+            <div style={{
+              backgroundColor: "#d8edce",
               padding: "40px 0",
               margin: "0 auto",
               borderRadius: "10px",
               color: "#7F4DFF",
-              width: "100%"
-            }}> {errorMessage}</p>
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "10px"
+            }}>
+              <span><i className="fa-solid fa-circle-check" style={{ fontSize: "40px", color: "#00A44F" }}></i></span>
+              <p style={{ color: "#00A44F", margin: "0" }}>
+                {successText}
+              </p>
+
+            </div>
           </div>
         </>
-    }
-  </>
+
+        :
+        <>
+          {
+            loadPermes === 200 ?
+              (<>
+                <Toaster />
+                {tabNumberContinues &&
+                  <div style={{ width: "100%", }}>
+                    <Input placeholder="Anketa ID" style={{ width: "100%", height: "39.5px", marginBottom: "10px" }} onChange={(e) => setTabNumber(e.target.value)} value={tabNumber} />
+                    <button disabled={Boolean(!tabNumber)} onClick={() => { setTabNumberContinues2(true), setTabNumberContinues(false) }} style={{ padding: "12px 20px", width: "100%", borderRadius: "10px", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", cursor: Boolean(!tabNumber) ? "not-allowed" : "pointer" }}>
+                      Davom etish <i className="fa-solid fa-arrow-right"></i>
+                    </button>
+                  </div>
+                }
+                {tabNumberContinues2 &&
+                  <div style={{ width: "100%", textAlign: "center", display: "flex", flexDirection: "column", gap: "5px" }}>
+                    <Breadcrumb
+                      items={items}
+                    />
+                    <h3 style={{ margin: "10px" }}>Holatlar</h3>
+                    {
+                      data?.results?.map((item: any) => (
+                        <div onClick={() => {
+                          if (item?.children) {
+                            setDataPathFilter(item?.id);
+                            setStatusID('');
+                          } else {
+                            setStatusID(item);
+                          }
+                        }} style={{ backgroundColor: statusID?.id === item?.id ? "rgba(111, 120, 127, 0.1)" : "", cursor: "pointer", width: '100%', border: "1px solid rgba(0,0,0,0.1) ", padding: "10px 0", marginBottom: "5px", borderRadius: "8px", position: "relative" }}>
+                          {item?.name}
+                          {item?.children && <span style={{ position: "absolute", top: "12px", right: "15px" }}><i className="fa-solid fa-chevron-down"></i></span>}
+                        </div>
+                      ))
+                    }
+                    {statusID?.id &&
+                      <>
+                        {(statusID?.requirement && statusID?.requirement !== "not_money_and_date") && <div style={{ width: "100%" }}>
+                          <DatePicker placeholder=" Sana tanlash" onChange={onChange} onFocus={() => setErrors((prev: any) => ({ ...prev, payment_date: "" }))} style={{ height: "39.5px", marginBottom: "5px", width: "100%" }} />
+                          {errors?.payment_date && <p style={{ color: "red", margin: "0", textAlign: "start" }}>{errors?.payment_date}</p>}
+                        </div>}
+
+                        {(statusID?.requirement && statusID?.requirement === "ten_day_in_month") && <div style={{ marginBottom: "10px" }}>
+                          <InputNumber onFocus={() => setErrors((prev: any) => ({ ...prev, payment_amount: "" }))} type="number" placeholder="To'lov summasi" value={price} onChange={(e) => setPrice(e)} style={{ height: "39.5px", marginBottom: "3px", width: "100%" }} />
+                          {errors?.payment_amount && <p style={{ color: "red", margin: "0", textAlign: "start" }}>{errors?.payment_amount}</p>}
+
+                        </div>}
+
+                        <div style={{ marginBottom: "10px" }}>
+                          <TextArea
+                            onFocus={() => setErrors((prev: any) => ({ ...prev, comment: "" }))}
+                            showCount
+
+                            onChange={(e) => setComment(e?.target?.value)}
+                            placeholder="Izoh 10 ta belgidan katta bo'lsin"
+                            style={{ height: 120, resize: 'none', marginBottom: "3px" }}
+                          />
+                          {errors?.comment && <p style={{ color: "red", margin: "0", textAlign: "start" }}>{errors?.comment}</p>}
+                        </div>
+
+                        <button disabled={loading} onClick={uploadToServer} style={{ padding: "12px 20px", width: "100%", borderRadius: "10px", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", cursor: loading ? "not-allowed" : "pointer" }}>
+                          {loading && <i className="fa-solid fa-spinner"></i>} Yuborish <i className="fa-solid fa-paper-plane"></i>
+                        </button>
+                      </>
+                    }
+                  </div>}
+
+                {
+                  <div style={{
+                    textAlign: "center",
+                    gap: "10px",
+                    width: "100%",
+                    display: (!tabNumberContinues2 && !tabNumberContinues) ? "grid" : "none"
+
+                  }}>
+                    {<video ref={videoRef} autoPlay playsInline style={{ width: "100%", display: (videoAllowed && !photoTaken) ? "block" : "none", borderRadius: "10px" }}></video>}
+
+                    {<canvas ref={canvasRef} style={{ width: "100%", display: photoTaken ? "block" : "none", borderRadius: "10px" }}></canvas>}
+
+                    {error && <p style={{ color: "red", }}>{error}</p>}
+                    {(!locationAllowed && !videoAllowed) && <p style={{
+                      backgroundColor: "#E5E5FF",
+                      padding: "20px 0",
+                      margin: "0 auto",
+                      borderRadius: "10px",
+                      color: "#7F4DFF",
+                      width: "100%"
+                    }}>
+                      Joylashuv ma'lumotlarini berishga rozimisiz?</p>}
+
+
+                    {(locationAllowed && videoAllowed) ? (
+                      <div style={{ display: "flex", gap: "10px", justifyContent: "center", width: "100%" }}>
+                        {photoTaken ? <>
+                          <button onClick={() => startCamera()} style={{ padding: "12px 20px", width: "100%", borderRadius: "10px", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", display: "flex", gap: "5px", justifyContent: "center", alignItems: "center", cursor: "pointer" }}>
+                            <i className="fa-solid fa-camera-rotate"></i> <span style={{ whiteSpace: "nowrap" }}>Kameraga qaytish</span>
+                          </button>
+                          <button onClick={() => { setTabNumberContinues(true), stopCamera() }} style={{ padding: "12px 20px", width: "100%", borderRadius: "10px", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", cursor: "pointer" }}>
+                            {!!tabNumber && <i className="fa-solid fa-spinner"></i>} Davom etish <i className="fa-solid fa-arrow-right"></i>
+                          </button>
+                        </> :
+                          <div style={{ display: "flex", justifyContent: "space-between", width: "100%", gap: "10px" }}>
+                            <button onClick={captureImage} style={{ padding: "12px 20px", borderRadius: "10px", width: "100%", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", cursor: "pointer" }}>
+                              <i className="fa-solid fa-camera-retro"></i> Rasm olish
+                            </button>
+                            <button onClick={() => {
+                              setToggle(toggle === "user" ? "environment" : "user");
+                              startCamera();
+                            }}
+
+                              style={{ padding: "12px 20px", borderRadius: "10px", width: "100%", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", cursor: "pointer", display: "flex", justifyContent: "center", gap: "5px" }}>
+                              <i className="fa-solid fa-camera-retro"></i> <span style={{ whiteSpace: "nowrap" }}>Kamerani almashtirish</span>
+                            </button>
+                          </div>
+                        }
+                      </div>
+                    ) :
+                      <button onClick={onPermissionChange} style={{ padding: "12px 20px", borderRadius: "10px", width: "100%", border: "none", backgroundColor: "#E5E5FF", color: "#7F4DFF", cursor: "pointer", display: "flex", gap: "5px", justifyContent: "center", alignItems: "center", }}>
+                        <i className="fa-solid fa-camera-rotate"></i> <span style={{ whiteSpace: "nowrap" }}>Ruxsat berish</span>
+                      </button>
+                    }
+                  </div>
+                }
+              </>) :
+              <>
+                <div style={{
+                  textAlign: "center",
+                  gap: "10px",
+                  width: "100%",
+                  display: "grid"
+
+                }}>
+                  <p style={{
+                    backgroundColor: "#E5E5FF",
+                    padding: "40px 0",
+                    margin: "0 auto",
+                    borderRadius: "10px",
+                    color: "#7F4DFF",
+                    width: "100%"
+                  }}> {errorMessage}</p>
+                </div>
+              </>
+          }
+        </>
+      }
+    </>
   );
 };
 
